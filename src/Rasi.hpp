96493aef497f8923023efafac2346ee7b23001ff
Sadaananth Anbucheliyan <ananth.anbucheliyan@gmail.com>
2023-12-14 02:40:44 +0800

    Added properties for RasiKattam

--- src/Rasi.hpp
+++ src/Rasi.hpp
@@ -2,7 +2,14 @@
 
 #include "Common.hpp"
 
+#include <vector>
+
 struct RasiBase
 {
     Rasi rasi;
+    uint8_t kattamIndex;
+    uint16_t degree;
+    Graham athibathi;
+    std::vector<Graham> utchaGraham;
+    std::vector<Graham> neetchaGraham;
 };
